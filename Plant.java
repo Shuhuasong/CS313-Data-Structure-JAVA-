653b6f81f408fec3bdfd422c884db23c6c0dc125
Shuhua Song <36608134+Shuhuasong@users.noreply.github.com>
2020-03-14 00:54:30 -0400

    Add files via upload

---
+++ Plant.java
@@ -0,0 +1,26 @@
+public class Plant {
+    protected String plantName;
+    protected String plantCost;
+
+    public void setPlantName(String userPlantName) {
+        plantName = userPlantName;
+    }
+
+    public String getPlantName() {
+        return plantName;
+    }
+
+    public void setPlantCost(String userPlantCost) {
+        plantCost = userPlantCost;
+    }
+
+    public String getPlantCost() {
+        return plantCost;
+    }
+
+    public void printInfo() {
+        System.out.println("Plant Information: ");
+        System.out.println("   Plant name: " + plantName);
+        System.out.println("   Cost: " + plantCost);
+    }
+}
